8ab624e15391b5ee0a86f80ae48267906ad36d65
Vladimir Elchinov <ev@gdelu.ru>
2014-04-05 17:36:48 +0400

    Bare rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sdr::Application.config.secret_key_base = '22df003c0dd4ab39c550b7dfc28529df2810ec6ddca2c689303e776be284e46dba6c3c2075923926a12a4f1bf3aa36baba7518a8d8bad397f6de23b4081fd3e7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sdr::Application.config.session_store :cookie_store, key: '_sdr_session'
